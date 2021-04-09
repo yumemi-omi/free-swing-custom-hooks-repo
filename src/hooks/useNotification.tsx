cf20b8845d3796aca7e7bae0c19735c267886470
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-04-10 00:51:17 +0900

    fix: 通知の設定を促すようにポップアップを表示させる

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -1,40 +1,39 @@
 import { useState, useEffect } from 'react'
 
-const notificationPermission = {
-  default: "default",
-  denied: "denied",
-  granted: "granted",
-};
-
 export const useNotification = (message: string) => {
   const [isGranted, setPermission] = useState(false)
 
   useEffect(() => {
-    function checkNotificationPromise() {
+    const checkNotificationPromise = () => {
       try {
-        Notification.requestPermission().then();
+        Notification.requestPermission().then()
       } catch(e) {
         return false;
       }
-
       return true;
+    }
+
+    const notify = () => {
+      new Notification(message)
+      setPermission(true)
     }
 
     // Let's check if the browser supports notifications
     if (!('Notification' in window)) {
       console.log("This browser does not support notifications.");
     } else {
-      if(checkNotificationPromise()) {
-        Notification.requestPermission()
-        .then((permission) => {
-          new Notification("new");
-          setPermission(true)
-        })
-      } else {
-        Notification.requestPermission(function(permission) {
-          new Notification("old");
-          setPermission(true)
-        });
+      if (Notification.permission === 'granted') {
+        notify()
+      } else if (Notification.permission !== 'denied') {
+        if(checkNotificationPromise()) {
+          Notification.requestPermission().then(() => {
+            notify()
+          })
+        } else {
+          Notification.requestPermission(function() {
+            notify()
+          });
+        }
       }
     }
   }, [])
