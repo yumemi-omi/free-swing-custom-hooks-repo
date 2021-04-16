6f75fb0ccc4433e1232b900432f92fb02b32da0a
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-04-16 21:52:02 +0900

    fix: locationをwatchする

--- src/components/root/Map.tsx
+++ src/components/root/Map.tsx
@@ -1,11 +1,12 @@
 import { useGeolocation } from '@/hooks/useGeolocation'
 
 const Map = () => {
-  const { location } = useGeolocation()
+  const { location, stopWatchLoaction } = useGeolocation()
   return (
     <div>
       <span>緯度{location.latitude}</span>
       <span>経度{location.longitude}</span>
+      <button onClick={stopWatchLoaction}>Stop watch location</button>
     </div>
   )
 }
--- src/hooks/useGeolocation.tsx
+++ src/hooks/useGeolocation.tsx
@@ -1,10 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 export const useGeolocation = () => {
   const [location, setLocation] = useState({
     latitude: 0,
     longitude: 0
   })
+  const [watchStatus, setWatchStatus] = useState({
+    isWatching: false, watchId: NaN
+  })
+
+  const stopWatchLoaction = useCallback(() => {
+    navigator.geolocation.clearWatch(watchStatus.watchId);
+    setWatchStatus({ isWatching: false, watchId: NaN });
+  }, [watchStatus.watchId])
+
   useEffect(() => {
     if('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -19,8 +28,20 @@ export const useGeolocation = () => {
     }
   }, [])
 
+  useEffect(() => {
+    if('geolocation' in navigator) {
+      const watchId = navigator.geolocation.watchPosition(position => {
+        const { latitude, longitude } = position.coords;
+        console.log({latitude, longitude})
+        setLocation({ latitude, longitude });
+      });
+      setWatchStatus({ isWatching: true, watchId });
+    }
+  }, [])
+
   return {
     location,
-    setLocation
+    setLocation,
+    stopWatchLoaction
   }
 }
