9c29bdbf19d453aca636cdf585afd22e110e8d8c
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-04-29 20:23:03 +0900

    fix: テキストエリアのリサイズちょっとまとめた

--- src/components/root/ResizeTextarea.tsx
+++ src/components/root/ResizeTextarea.tsx
@@ -1,58 +1,35 @@
-import {
-  useState,
-  useCallback,
-  useRef,
-  useEffect,
-  useLayoutEffect,
-  VFC,
-} from 'react';
+import { useState, useCallback, useRef, useEffect, VFC } from 'react';
 import { css } from '@emotion/react';
 
 const ALLOWANCE = 2;
 
 const ResizeTextarea: VFC = () => {
   const [text, setText] = useState('');
-  const heightRef = useRef('');
   const ref = useRef<HTMLTextAreaElement>(null);
+  const heightRef = useRef('');
 
   const onChange = useCallback<React.ChangeEventHandler<HTMLTextAreaElement>>(
     (e) => {
       setText(e.target.value);
-      // resizeTextarea();
     },
     []
   );
-
-  const resizeTextarea = useCallback(() => {
-    heightRef.current = 'auto';
-    if (ref && ref.current) {
-      const el = ref.current;
-      heightRef.current = `${el.scrollHeight + ALLOWANCE}px`;
-    }
-  }, [heightRef]);
 
   useEffect(() => {
     if (ref && ref.current) {
       const el = ref.current;
       el.style.height = 'auto';
-      // heightRef.current = 'auto';
-      el.style.height = `${el.scrollHeight + ALLOWANCE}px`;
-      // heightRef.current = `${el.scrollHeight}px`;
+      const height = `${el.scrollHeight + ALLOWANCE}`;
+      if (height !== heightRef.current) {
+        el.style.height = `${height}px`;
+        heightRef.current = `${height}px`;
+      }
     }
   }, [text]);
-
-  // useEffect(() => {
-  //   if (ref && ref.current) {
-  //     setHeight('auto');
-  //     const el = ref.current;
-  //     setHeight(`${el.scrollHeight + ALLOWANCE}px`);
-  //   }
-  // }, [text]);
 
   return (
     <textarea
       css={textareaStyle}
-      style={{ height: heightRef.current }}
       value={text}
       name=""
       rows={1}
