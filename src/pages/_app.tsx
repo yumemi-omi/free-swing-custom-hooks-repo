4f544d06a1b51439e83572aea85bf5c1995f53cc
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-05-06 23:05:13 +0900

    feat: mswを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import { GlobalStyles } from '@/styles/Globals';
 import type { AppProps } from 'next/app';
 import DarkThemeProvider from '@/providers/darkThemeProvider';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../../.mocks');
+}
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
