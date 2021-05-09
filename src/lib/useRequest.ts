d480345aa51cc4ea79d4e8273f09289378885d8e
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-05-09 10:41:29 +0900

    fix サンプルから引用

--- src/lib/useRequest.ts
+++ src/lib/useRequest.ts
@@ -1,31 +1,29 @@
-import useSWR, { ConfigInterface, responseInterface } from 'swr';
+import useSWR, { SWRConfiguration, SWRResponse } from 'swr';
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
-// .envにのせる
-const AxiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  // headers: { Authorization: `Bearer ${token}` }, // tokenはなんらかの形でセットしてください
-});
-
-export type RequestType = AxiosRequestConfig | null;
+export type GetRequest = AxiosRequestConfig | null;
 
 interface Return<Data, Error>
   extends Pick<
-    responseInterface<AxiosResponse<Data>, AxiosError<Error>>,
+    SWRResponse<AxiosResponse<Data>, AxiosError<Error>>,
     'isValidating' | 'revalidate' | 'error' | 'mutate'
   > {
+  data: Data | undefined;
   response: AxiosResponse<Data> | undefined;
 }
 
-export type Config<Data = unknown, Error = unknown> = Omit<
-  ConfigInterface<AxiosResponse<Data>, AxiosError<Error>>,
-  ''
->;
+export interface Config<Data = unknown, Error = unknown>
+  extends Omit<
+    SWRConfiguration<AxiosResponse<Data>, AxiosError<Error>>,
+    'initialData'
+  > {
+  initialData?: Data;
+}
 
-export const useRequest = <Data = unknown, Error = unknown>(
-  request: RequestType,
-  config: Config<Data, Error> = {}
-): Return<Data, Error> => {
+export default function useRequest<Data = unknown, Error = unknown>(
+  request: GetRequest,
+  { initialData, ...config }: Config<Data, Error> = {}
+): Return<Data, Error> {
   const { data: response, error, isValidating, revalidate, mutate } = useSWR<
     AxiosResponse<Data>,
     AxiosError<Error>
@@ -36,14 +34,26 @@ export const useRequest = <Data = unknown, Error = unknown>(
      * function is actually only called by `useSWR` when it isn't.
      */
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    () => AxiosClient(request!),
-    config
+    () => axios(request!),
+    {
+      ...config,
+      initialData: initialData && {
+        status: 200,
+        statusText: 'InitialData',
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        config: request!,
+        headers: {},
+        data: initialData,
+      },
+    }
   );
+
   return {
+    data: response && response.data,
     response,
     error,
     isValidating,
     revalidate,
     mutate,
   };
-};
+}
